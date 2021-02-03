ccb8a77fee7cee99e9e143178d2c761e04ad27a0
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-02-03 15:30:37 -0800

    almost to routing

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
-    <p>
-      welcome to pats cookie store we sell salmon flavored cookies to unsuspecting tourests.
-    </p>
-    <h3> See our sales data</h3>
-    <ul>
-      <li><a href="/salesData" target="_blank" rel="noopener">sales Data</a></li>
-    </ul>
-
-    <h3>About Our stores</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-    
+    <Home />
+    <StoreData />    
   </div>
 </template>
 
+
+// should render either home or salesData based on routing
+
+
 <script>
+import Home from '../Home/Home.vue'
+import StoreData from '../Data/Data.vue'
+
 export default {
-  name: 'HelloWorld',
+  name: 'Main',
+  components: {
+    Home,
+    StoreData
+  },
   props: {
     msg: String
   }
