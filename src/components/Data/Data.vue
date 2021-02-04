5ede9b3729e01f99bd4953baa7dc72cc4e7e5dc1
garrett <grrttcintron@gmail.com>
2021-02-03 20:49:29 -0800

    pushing code for night working on data parsing

--- src/components/Data/Data.vue
+++ src/components/Data/Data.vue
@@ -1,29 +1,57 @@
 <template>
   <div class="storeData">
-        <h2>About Our stores</h2>
-        <form>
-        <input v-model="location" placeholder="store Location">
-        <input v-model="MinCust" placeholder="Min customers">
-        <input v-model="MaxCust" placeholder="Max customers">
-        <input v-model="AvgSales" placeholder="Average cookie sales">
+    <h1>{{ $log(seattle) }}</h1>
+    <h2>About Our stores</h2>
+    <form>
+      <input v-model="location" placeholder="store Location" />
+      <input v-model="MinCust" placeholder="Min customers" />
+      <input v-model="MaxCust" placeholder="Max customers" />
+      <input v-model="AvgSales" placeholder="Average cookie sales" />
 
-        <button> submit</button>
-        </form>
-
-    <p>DATA SHOULD BE DISPLAYED HERE</p>
- 
+      <button>submit</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'StoreData',
+  name: "StoreData",
   props: {
-    
-  }
-}
+    location: String,
+    MinCust: Number,
+    MaxCust: Number,
+    AvgSales: Number,
+  },
+};
 </script>
 
+<script>
+import Store from './constructor.vue'
+
+export const storeMaker = {
+  
+  data: () => ({
+    store: new Store()
+  }),
+
+  methods: {
+    async createStore() {
+      Object.assign(this.store, new Store())
+    },
+
+    log() {
+      console.log()
+    }
+  }
+}
+  
+  
+ 
+
+</script>
+
+<script>
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
---
+++ src/components/Data/constructor.vue
@@ -0,0 +1,27 @@
+<script>
+import R from "ramda"
+
+export default class Store {
+  constructor(store = {}) {
+    this.location = R.is(string, store.location) ? location : "";
+    this.MinCust = R.is(number, store.MinCust) ? MinCust : 0;
+    this.MaxCust = R.is(number, store.MaxCust) ? MaxCust : 0;
+    this.AvgSales = R.is(number, store.AvgSales) ? AvgSales : 0;
+  }
+
+  getName() {
+    return this.location;
+  }
+
+  createStore() {
+    return {
+      location: this.location,
+      MinCust: this.MinCust,
+      MaxCust: this.MaxCust,
+      AvgSales: this.AvgSales,
+    }
+  }
+
+}
+
+</script>
